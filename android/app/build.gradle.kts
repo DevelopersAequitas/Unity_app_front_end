334b0b6de2eea9f0dcd2763ca54b5ff917825d15
Chirag Mali <ck.futuretech@gmail.com>
2026-09-23 14:07:34 +0530

    fix: enable coreLibraryDesugaring and resValues in android build config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,12 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+    buildFeatures {
+        resValues = true
+    }
+
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -59,4 +64,8 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
